7a3928ab68d10df2021000d3339f215d6452b558
AJAY VENKATESH BABU <ajayvengateshbabu@gmail.com>
2024-10-06 22:04:06 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+const squares = document.querySelectorAll(".square");
+
+// Add event listeners for mouseover and mouseout
+squares.forEach((element) => {
+    element.addEventListener("mouseover", handleMouseOver);
+    element.addEventListener("mouseout", handleMouseOut);
+});
+
+function handleMouseOver(event) {
+    squares.forEach((element) => {
+        if (element !== event.target) {
+            element.style.backgroundColor = "#6F4E37"; // Change color of other squares
+        }
+    });
+}
+
+function handleMouseOut() {
+    squares.forEach((square) => {
+        square.style.backgroundColor = "#E6E6FA"; // Reset color
+    });
+}
